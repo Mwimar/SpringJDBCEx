5156c3c7f7accb4bc3e244e58cf6efb7330219b4
Mark Mwima <markmwimar@gmail.com>
2024-08-23 16:00:00 +0300

    save method

--- src/main/java/com/mwimar/SpringJDBCEx/repo/StudentRepo.java
+++ src/main/java/com/mwimar/SpringJDBCEx/repo/StudentRepo.java
@@ -1,4 +1,12 @@
 package com.mwimar.SpringJDBCEx.repo;
 
+import com.mwimar.SpringJDBCEx.model.Student;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class StudentRepo {
+
+    public void save(Student s) {
+        System.out.println("Student saved");
+    }
 }
--- src/main/java/com/mwimar/SpringJDBCEx/service/StudentService.java
+++ src/main/java/com/mwimar/SpringJDBCEx/service/StudentService.java
@@ -2,12 +2,25 @@ package com.mwimar.SpringJDBCEx.service;
 
 
 import com.mwimar.SpringJDBCEx.model.Student;
+import com.mwimar.SpringJDBCEx.repo.StudentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
 
+    StudentRepo repo;
+
+    public StudentRepo getRepo() {
+        return repo;
+    }
+
+    @Autowired
+    public void setRepo(StudentRepo repo) {
+        this.repo = repo;
+    }
+
     public void addStudent(Student s){
-        System.out.println("Added student");
+        repo.save(s);
     }
 }
