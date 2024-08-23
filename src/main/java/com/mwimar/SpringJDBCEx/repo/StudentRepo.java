d930674b5d416520fc6b0f34f436340884383cbd
Mark Mwima <markmwimar@gmail.com>
2024-08-23 16:10:52 +0300

    find all method

--- src/main/java/com/mwimar/SpringJDBCEx/repo/StudentRepo.java
+++ src/main/java/com/mwimar/SpringJDBCEx/repo/StudentRepo.java
@@ -3,10 +3,18 @@ package com.mwimar.SpringJDBCEx.repo;
 import com.mwimar.SpringJDBCEx.model.Student;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public class StudentRepo {
 
     public void save(Student s) {
         System.out.println("Student saved");
+    }
+
+    public List<Student> findAll() {
+        List<Student> students = new ArrayList<>();
+        return students;
     }
 }
--- src/main/java/com/mwimar/SpringJDBCEx/service/StudentService.java
+++ src/main/java/com/mwimar/SpringJDBCEx/service/StudentService.java
@@ -6,6 +6,8 @@ import com.mwimar.SpringJDBCEx.repo.StudentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class StudentService {
 
@@ -22,5 +24,9 @@ public class StudentService {
 
     public void addStudent(Student s){
         repo.save(s);
+    }
+
+    public List<Student> getStudents() {
+        return repo.findAll();
     }
 }
