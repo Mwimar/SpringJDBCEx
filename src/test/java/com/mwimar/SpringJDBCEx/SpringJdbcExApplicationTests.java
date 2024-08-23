643e8d05bd0cd4eb60cac207f26a7c1dc134c310
Mark Mwima <markmwimar@gmail.com>
2024-08-23 13:24:36 +0300

    spring jdbc intro

---
+++ src/test/java/com/mwimar/SpringJDBCEx/SpringJdbcExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwimar.SpringJDBCEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
