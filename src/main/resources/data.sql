5ac7cbca43d6d9481961fe77fbf5fffd6ef2df74
Mark Mwima <markmwimar@gmail.com>
2024-08-24 21:57:51 +0300

    postgresql dependency

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into student (rollno, name, marks) values(10, 'Kevin', 79);
-insert into student (rollno, name, marks) values(20, 'Starboy', 68);
-insert into student (rollno, name, marks) values(30, 'Kartel', 84);
+--insert into student (rollno, name, marks) values(10, 'Kevin', 79);
+--insert into student (rollno, name, marks) values(20, 'Starboy', 68);
+--insert into student (rollno, name, marks) values(30, 'Kartel', 84);
